7d6cfb08eee91e1d0c762d1c1ecb02844aa9f1d3
Kostya Paranko <curvhand@gmail.com>
2025-11-09 07:58:35 +1000

    with stars

--- tests/settings.go
+++ tests/settings.go
@@ -1,6 +1,19 @@
 package tests
 
-var Port = 7540
+import (
+	"os"
+	"strconv"
+)
+
+var Port = func() int {
+	if v := os.Getenv("TODO_PORT"); v != "" {
+		if p, err := strconv.Atoi(v); err == nil {
+			return p
+		}
+	}
+	return 7540
+}()
+
 var DBFile = "../scheduler.db"
 var FullNextDate = false
 var Search = false
